71370ed3680885962c8a88177da79bd79e3cfc7c
Jassi <simran5590@gmail.com>
2015-07-20 21:45:09 -0700

    Getting started with ASP.Net Web API – Part 3

---
+++ LearningWeb/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using eLearning.Context;
+using eLearning.Entities;
+using eLearning.Repository;
+
+namespace LearningWeb.Controllers
+{
+    public class CoursesController : ApiController
+    {
+        public List<Course> Get()
+        {
+            ILearningRepository repository = new LearningRepository(new LearningContext());
+            return repository.GetAllCourses().ToList();
+        }
+
+        public Course GetCourse(int id)
+        {
+            ILearningRepository repository = new LearningRepository(new LearningContext());
+
+            return repository.GetCourse(id);
+        }
+    }
+}
